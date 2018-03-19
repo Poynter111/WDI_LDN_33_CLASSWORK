2c90d438da839fcfbeeeb4f49b7817b704c2f521
Poynter111 <alex.poynter111@gmail.com>
2018-03-19 17:06:06 +0000

    Javascript hex-decimal

---
+++ Poynter111/w02d01/starter-code/js/app.js
@@ -0,0 +1,27 @@
+// Color converter
+let hex = prompt("Please enter a HEX code in the format RRGGBB")
+
+
+
+
+
+// var rgbToHex = function (rgb) {
+//   var hex = Number(rgb).toString(16);
+//   if (hex.length < 2) { hex = "0" + hex;
+//   }
+//   return hex;
+// };
+//
+// var fullColorHex = function(r,g,b) {
+//   var red = rgbToHex(r);
+//   var green = rgbToHex(g);
+//   var blue = rgbToHex(b);
+//   return red+green+blue;
+// };
+// FizzBuzz
+
+// Guess the number
+
+// Password strength checker
+
+// Mastermind
