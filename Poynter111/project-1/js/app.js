dfe68f93d8d37501d226e977fb1f1056f6669874
Poynter111 <alex.poynter111@gmail.com>
2018-04-03 10:47:06 +0100

    towers no place in correct way

--- Poynter111/project-1/js/app.js
+++ Poynter111/project-1/js/app.js
@@ -1,50 +1,10 @@
 $(function(){
   //----------------------Map blueprint & Controls--------------------------
-  var gameBoard =[
-    [0,0,1,1,1,1,1,1,1,6],
-    [0,0,1,0,0,0,0,0,0,0],
-    [0,0,1,1,1,1,1,1,0,0],
-    [0,0,0,0,0,0,0,1,0,0],
-    [0,0,0,0,1,1,1,1,0,0],
-    [0,0,0,0,1,0,0,0,0,0],
-    [0,1,1,1,1,0,0,0,0,0],
-    [0,1,0,0,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0,0,7,0]
-  ];
+
   var $mapWrap = $('.map-wrap');
   var $controls = $('.controls');
   //-----------------------------------TOWERS-----------------------------------
-  var $towers =
-{
-  $tower1:
-    {
-      name: 'Machine',
-      hTml: $('#1'),
-      value: 50,
-      range: 5,
-      class: 'tower1',
-      dmg: 10
-    },
-  $tower2:
-    {
-      name: 'Sniper',
-      hTml: $('#2'),
-      value: 100,
-      range: 10,
-      class: 'tower2',
-      dmg: 50
-    },
-  $tower3:
-  {
-    name: '50 Cal',
-    hTml: $('#3'),
-    value: 150,
-    range: 5,
-    class: 'tower3',
-    dmg: 30
-  }
-};
+
 
   //---------------------------Map Generation-----------------------------------
   function mapBuilder(){
@@ -73,13 +33,13 @@ $(function(){
   function controlsFn(){
     $controls.children('.cntrBtn').each(function(){
       if($(this).attr('id') === '1'){
-        $(this).text($towers.$tower1.name);
+        $(this).text(towers.$tower1.name);
       }
       if($(this).attr('id') === '2'){
-        $(this).text($towers.$tower2.name);
+        $(this).text(towers.$tower2.name);
       }
       if($(this).attr('id') === '3'){
-        $(this).text($towers.$tower3.name);
+        $(this).text(towers.$tower3.name);
       }
       // if($(this).attr('id') === '4'){  Need to decide what these buttone will do
       //   $(this).text($tower3.name);      will there function stay here or
@@ -97,60 +57,32 @@ $(function(){
       $.each(line, (j, cell) => {
         if (cell === 0){
           $(`.cell_${i}_${j}`).addClass('availableTile');
-          event(btnClicked);
         }
       });
     });
+    event(btnClicked);
   });
   function event(btnClicked){
-    $mapWrap.children('.availableTile').on('click', function(){
-      $(this).addClass('tower1');
-      console.log(btnClicked);
+
+    $mapWrap.children('.availableTile').on('click', function(event){
+      console.log(event);
+      for(key in towers){
+        if (towers[key].name === btnClicked){
+          var selectedT  = towers[key].class;
+          $(this).addClass(selectedT);
+          $mapWrap.children('.availableTile').off('click');
+          $mapWrap.children().removeClass('availableTile');
+        }
+      }
     });
   }
+  // console.log(towers[key].name);
+  //[key]
+  //,
   // $( "p" ).click(function() {
   //   var htmlString = $( this ).html();
   //   $( this ).text( htmlString );
   // });
-
-
-  // $tower1.hTml.on('click', function(){
-  //   $.each(gameBoard, function(i, line){
-  //     $.each(line, (j, cell) => {
-  //       if (cell === 0){
-  //         $(`.cell_${i}_${j}`).addClass('availableTile');
-  //         $mapWrap.children('.availableTile').on('click', function(){
-  //           $(this).addClass('tower1');
-  //         });
-  //       }
-  //     });
-  //   });
-  // });
-  // $tower2.on('click', function(){
-  //   $.each(gameBoard, function(i, line){
-  //     $.each(line, (j, cell) => {
-  //       if (cell === 0){
-  //         $(`.cell_${i}_${j}`).addClass('availableTile');
-  //         $mapWrap.children('.availableTile').on('click', function(){
-  //           $(this).addClass('tower2');
-  //         });
-  //       }
-  //     });
-  //   });
-  // });
-  // $tower3.on('click', function(){
-  //   $.each(gameBoard, function(i, line){
-  //     $.each(line, (j, cell) => {
-  //       if (cell === 0){
-  //         $(`.cell_${i}_${j}`).addClass('availableTile');
-  //         $mapWrap.children('.availableTile').on('click', function(){
-  //           $(this).addClass('tower3');
-  //         });
-  //       }
-  //     });
-  //   });
-  // });
-
 
   // function reset(){
   //   $mapWrap.children('.availableTile').removeClass('.availableTile');
